3d1e3b09baa758be3fb128ce1e40e29185b0d578
fmazurek-mm <student-03-421e30eb0809@qwiklabs.net>
2026-05-28 08:00:16 +0000

    rollout

--- main.go
+++ main.go
@@ -11,7 +11,7 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License. rata
+ * limitations under the License.
  */
 
 package main
@@ -26,20 +26,12 @@ import (
 
 func main() {
 	http.HandleFunc("/blue", blueHandler)
-	http.HandleFunc("/red", redHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
 func blueHandler(w http.ResponseWriter, r *http.Request) {
 	img := image.NewRGBA(image.Rect(0, 0, 100, 100))
 	draw.Draw(img, img.Bounds(), &image.Uniform{color.RGBA{0, 0, 255, 255}}, image.ZP, draw.Src)
-	w.Header().Set("Content-Type", "image/png")
-	png.Encode(w, img)
-}
-
-func redHandler(w http.ResponseWriter, r *http.Request) {
-	img := image.NewRGBA(image.Rect(0, 0, 100, 100))
-	draw.Draw(img, img.Bounds(), &image.Uniform{color.RGBA{255, 0, 0, 255}}, image.ZP, draw.Src)
 	w.Header().Set("Content-Type", "image/png")
 	png.Encode(w, img)
 }
